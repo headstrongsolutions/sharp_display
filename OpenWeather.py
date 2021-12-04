a4687700c16637d530277c7bd41a98535029ab31
Chris.Morse <chris.morse@qinetiq.com>
2021-12-04 16:03:47 +0000

    Added OpenWeather placeholder

---
+++ OpenWeather.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- Mode: python; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+#
+# Copyright (C)        : 2021 Headstrong Solutions
+# Author:              : Chris Morse <chris@headstrong.solutions>
+#
+# Name                 : Open Weather
+
+"""OpenWeather module"""
