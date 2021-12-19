f5875f94a6f1da059fd340c88049b3d515619b21
Chris.Morse <chris.morse@qinetiq.com>
2021-12-19 12:33:23 +0000

    Min and Max 24 and week

--- Jarvis.py
+++ Jarvis.py
@@ -12,7 +12,8 @@
 import os
 import requests
 import datetime
-import json
+
+
 
 class Jarvis:
     def __init__(self):
@@ -20,6 +21,11 @@ class Jarvis:
             raise EnvironmentError(f'Failed because JARVIS_TEMPS_URL envar is not set')
         self.temps_url = os.environ.get('JARVIS_TEMPS_URL')
         self.latest_temperature = 0
+        self.temperatures = []
+        self.min_temp_24h = 0
+        self.max_temp_24h = 0
+        self.min_temp_week = 0
+        self.max_temp_week = 0
         self.next_access = datetime.datetime.now() 
 
     def get_temps(self):
@@ -28,9 +34,20 @@ class Jarvis:
             if self.next_access < current_time:
                 response = requests.get(self.temps_url)
                 response.raise_for_status()
-                jsonResponse = response.json()
-                self.latest_temperature = jsonResponse[0]['Value']
+                json_response = response.json()
+                self.latest_temperature = json_response[0]['Value']
+                self.temperatures = json_response
+                record_count = 0
+                for record in self.temperatures:
+                    for k, v in record.items():
+                        if k == 'Value':
+                            self.temperatures[record_count]['Value'] = float(v)
+                    record_count +=1
                 self.next_access = current_time + datetime.timedelta(minutes=5)
+                self.min_temp_24h = min(self.temperatures[0:288], key=lambda x:x['Value'])['Value']
+                self.max_temp_24h = max(self.temperatures[0:288], key=lambda x:x['Value'])['Value']
+                self.min_temp_week = min(self.temperatures, key=lambda x:x['Value'])['Value']
+                self.max_temp_week = max(self.temperatures, key=lambda x:x['Value'])['Value']
 
         except requests.HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
--- test_jarvis.py
+++ test_jarvis.py
@@ -4,3 +4,5 @@ def test_recieves_temps():
     jarvis = Jarvis()
     temps = jarvis.get_temps()
     assert temps is not None
+    assert jarvis.min_temp != 0
+    assert jarvis.max_temp != 0
