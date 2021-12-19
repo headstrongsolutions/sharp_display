a8707d7548d8020edf82d498b4c7bea6d0ad430e
Chris.Morse <chris.morse@qinetiq.com>
2021-12-19 12:33:46 +0000

    Missing test tweaks for 24h and week max and min test

--- test_jarvis.py
+++ test_jarvis.py
@@ -4,5 +4,7 @@ def test_recieves_temps():
     jarvis = Jarvis()
     temps = jarvis.get_temps()
     assert temps is not None
-    assert jarvis.min_temp != 0
-    assert jarvis.max_temp != 0
+    assert jarvis.min_temp_24h != 0
+    assert jarvis.max_temp_24h != 0
+    assert jarvis.min_temp_week != 0
+    assert jarvis.max_temp_week != 0
