7311c79cd6d96d18d8c70042976d1067a1c7dd7e
bonce <chris@headstrong.solutions>
2022-09-25 20:43:37 +0100

    CalDav and Xandikos guarding

--- caldav_calendar.py
+++ caldav_calendar.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from datetime import datetime, date, timedelta
+from time import sleep
+from datetime import datetime, timezone, timedelta
 from events import Events
 
 ## We'll try to use the local caldav library, not the system-installed
@@ -13,6 +14,7 @@ import caldav
 class CalDav_Calendar:
     def __init__(self):
         self.events = []
+        self.get_attempt = 0
 
         if os.environ.get('CALENDARURL') is None:
             raise EnvironmentError(f'Failed because CALENDARURL envar is not set')
@@ -37,32 +39,49 @@ class CalDav_Calendar:
                             ) as client:
             my_principal = client.principal()
             self.calendars = my_principal.calendars()
+            self.get_attempt += 1
 
-        self.calendar = self.calendars[0]
-        if self.calendar is None:
+        if self.calendars is None and self.get_attempt < 10:
+            sleep(1)
             self.get_calendar()
+        else:
+            self.get_attempt = 0
+            self.calendar = self.calendars[0]
+
         today = datetime.today()
         week_start = today - timedelta(days=today.weekday())
         then = week_start + timedelta(30)
 
         events = Events(week_start, then) 
+        self.month_events = self.calendar.date_search(start=week_start, end=then)
 
-        self.month_events = self.calendar.date_search(week_start, then)
-
-        for event in self.month_events:
-            summary = event.icalendar_instance.subcomponents[0]['SUMMARY']
-            day = event.icalendar_instance.subcomponents[0]['DTSTART'].dt.day
-            month = event.icalendar_instance.subcomponents[0]['DTSTART'].dt.month
-            year = event.icalendar_instance.subcomponents[0]['DTSTART'].dt.year
-            start = event.icalendar_instance.subcomponents[0]['DTSTART'].dt
-            duration = event.icalendar_instance.subcomponents[0]['DURATION']
-
-            start_dt = start
-            if start and duration: 
+        for event_collection in self.month_events:
+            summary = ""
+            start = None
+            end = None
+            duration = ""
+            event = {'SUMMARY':'', 'DESCRIPTION':'', 'DTSTART':'', 'DTEND':'', 'DURATION':''}
+            for event_list in event_collection.icalendar_instance.subcomponents:
+                for event_item in event_list:
+                    if event_item in event:
+                        event[event_item] = event_list[event_item]
+            if 'SUMMARY' in event:
+                summary = event['SUMMARY']
+            if not summary and 'DESCRIPTION' in event:
+                summary = event['DESCRIPTION']
+            if 'DTSTART' in event:
+                start = event['DTSTART'].dt
+            if 'DTEND' in event:
+                end = event['DTEND'].dt
+            if 'DURATION' in event:
+                duration = event['DURATION']
+            if start and end:
+                end_dt = end
+            elif start and duration: 
                 end_dt = start + duration.dt
             
-            if start_dt and end_dt:
-                events.add_event(start=start_dt, end=end_dt, title=summary, description=None)
+            #if start and end_dt:
+            events.add_event(start=start, end=end_dt, title=summary, description=None)
         self.events = events
 
     def print_events(self):
@@ -70,8 +89,15 @@ class CalDav_Calendar:
             for event in self.events.events:
                 duration = ""
                 if event.start is not None and event.end is not None and event.start != event.end:
-                    starttime = event.start.strftime("%H:%M")
-                    endtime = event.end.strftime("%H:%M")
+                    if isinstance(event.start, datetime):
+                        starttime = event.start.strftime("%H:%M")
+                    if isinstance(event.end, datetime):
+                        endtime = event.end.strftime("%H:%M")
                     if starttime != endtime:
                         duration  = (f"({starttime} - {endtime})") 
                 print(f"{event.title}: {Events.get_day_from_dt(event, event.start)} {duration}")
+
+if __name__ == "__main__":
+
+    caldav = CalDav_Calendar()
+    caldav.print_events()
